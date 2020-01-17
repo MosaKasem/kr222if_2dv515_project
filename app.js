1bd56510c170edb0ab86faee880e0740648408f7
MosaKasem <thescorpion119@gmail.com>
2020-01-17 23:33:47 +0100

    links done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Scrape = require('./lib/Scrape')
 
 const init = () => {
-  const scrape = new Scrape(1)
+  const scrape = new Scrape(30)
   // const scrape = new Scrape(262)
   // const scrape = new Scrape(200)
   scrape.scrape()
