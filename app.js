2f95aefbad392219450b01aa9a06e5173cf10f43
MosaKasem <thescorpion119@gmail.com>
2020-01-18 17:14:45 +0100

    found the issue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Scrape = require('./lib/Scrape')
 
 const init = () => {
-  const scrape = new Scrape(30)
+  const scrape = new Scrape(200)
   // const scrape = new Scrape(262)
   // const scrape = new Scrape(200)
   scrape.scrape()
